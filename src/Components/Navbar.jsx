c30f42f4c4cca17c3dc05f3f5c71f7d5fdf487c3
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-18 16:58:53 +0600

    updated mavbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,13 @@
-import navImg from "../assets/more/15.jpg";
 import bgImg from "../assets/more/15.jpg";
 import logo1 from "../assets/more/logo1.png";
 
 const Navbar = () => {
     return (
         <div
-            className="w-full h-25 py-3 text-white bg-no-repeat bg-cover"
+            className="w-full h-[100px] py-3 text-white bg-center bg-no-repeat bg-cover"
             style={{ backgroundImage: `url(${bgImg})` }}
         >
             <div className="max-w-7xl mx-auto flex justify-center items-center gap-3">
-                <img src={navImg} alt="nav logo" className="w-12 h-10 object-cover" />
                 <img src={logo1} alt="logo1" className="w-18 h-18 object-contain" />
                 <h1 style={{ fontFamily: "Rancho, cursive" }} className="text-5xl">
                     Espresso Emporium
