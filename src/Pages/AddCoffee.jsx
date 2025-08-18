3e5e8e9e228e830936f0ea1e6a6aec9b0e798d22
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-08-18 17:47:28 +0600

    updated footer section

--- src/Pages/AddCoffee.jsx
+++ src/Pages/AddCoffee.jsx
@@ -1,9 +1,11 @@
+import { FaArrowLeft } from "react-icons/fa6";
 const AddCoffee = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-500">
+        <div className="bg-[#F4F3F0]">
+            <button className="btn btn-ghost bg-transparent text-xl text-cyan-600 ml-5 mt-5 text-center" style={{ fontFamily: "Rancho, cursive" }}><FaArrowLeft /> Back To Home</button>
             <form className="flex justify-center">
-                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-                    <legend className="fieldset-legend text-lg font-semibold">Coffee Details</legend>
+                <fieldset className="fieldset bg-gray-400 border-base-300 rounded-box w-xl border p-15">
+                    <legend className="fieldset-legend text-lg font-semibold pt-10">Coffee Details</legend>
 
                     <label className="label">Name</label>
                     <input type="text" className="input input-bordered w-full" placeholder="Enter Coffee Name" />
